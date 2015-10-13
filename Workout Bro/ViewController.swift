92e589bd56e269f4fba0fd5f915f2c1959547e2a
julmaldo <julmaldo>
2015-10-12 20:03:34 -0500

    Initial Commit

---
+++ Workout Bro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Workout Bro
+//
+//  Created by Julio Maldonado on 10/12/15.
+//  Copyright © 2015 JEC INC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
