3d61aca59cdd7cafed885a08d947cceaf131fc69
Julio Maldonado <Julio.A.Maldo@gmail.comcd>
2015-10-16 16:23:53 -0500

    ui update

--- Workout Bro/ViewController.swift
+++ Workout Bro/ViewController.swift
@@ -7,19 +7,77 @@
 //
 
 import UIKit
+import EventKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1
+        let eventStore = EKEventStore()
+        
+        // 2
+        switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
+        case .Authorized:
+            insertEvent(eventStore)
+        case .Denied:
+            println("Access denied")
+        case .NotDetermined:
+            // 3
+            eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion:
+                {[weak self] (granted: Bool, error: NSError!) -> Void in
+                    if granted {
+                        self!.insertEvent(eventStore)
+                    } else {
+                        println("Access denied")
+                    }
+                })
+        default:
+            println("Case Default")
+        }
+    }
     }
 
-    override func didReceiveMemoryWarning() {
+    func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+func insertEvent(store: EKEventStore) {
+    // 1
+    let calendars = store.calendarsForEntityType(EKEntityTypeEvent)
+        as! [EKCalendar]
+    
+    for calendar in calendars {
+        // 2
+        if calendar.title == "ioscreator" {
+            // 3
+            let startDate = NSDate()
+            // 2 hours
+            let endDate = startDate.dateByAddingTimeInterval(2 * 60 * 60)
+            
+            // 4
+            // Create Event
+            var event = EKEvent(eventStore: store)
+            event.calendar = calendar
+            
+            event.title = "New Meeting"
+            event.startDate = startDate
+            event.endDate = endDate
+            
+            // 5
+            // Save Event in Calendar
+            var error: NSError?
+            let result = store.saveEvent(event, span: EKSpanThisEvent, error: &error)
+            
+            if result == false {
+                if let theError = error {
+                    println("An error occured \(theError)")
+                }
+            }
+        }
+    }
 }
+
 
