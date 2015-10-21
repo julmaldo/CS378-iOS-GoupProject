fe1634575d780ed47f83624326323668bb04e1e7
Julio Maldonado <Julio.A.Maldo@gmail.comcd>
2015-10-20 19:35:49 -0500

    rough ui design completed

--- Workout Bro/ViewController.swift
+++ Workout Bro/ViewController.swift
@@ -11,73 +11,23 @@ import EventKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var workoutButton: UIButton!
+    @IBOutlet weak var calendarButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 1
-        let eventStore = EKEventStore()
-        
-        // 2
-        switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
-        case .Authorized:
-            insertEvent(eventStore)
-        case .Denied:
-            println("Access denied")
-        case .NotDetermined:
-            // 3
-            eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion:
-                {[weak self] (granted: Bool, error: NSError!) -> Void in
-                    if granted {
-                        self!.insertEvent(eventStore)
-                    } else {
-                        println("Access denied")
-                    }
-                })
-        default:
-            println("Case Default")
+        self.workoutButton.layer.cornerRadius = 10.0
+        self.calendarButton.layer.cornerRadius = 10.0
+        let firstLaunch = NSUserDefaults.standardUserDefaults().boolForKey("FirstLaunch")
+        if firstLaunch  {
+            print("Not first launch.")
         }
-    }
-    }
+        else {
+            print("First launch, setting NSUserDefault.")
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "FirstLaunch")
+        }
+        
+       }
 
-    func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-func insertEvent(store: EKEventStore) {
-    // 1
-    let calendars = store.calendarsForEntityType(EKEntityTypeEvent)
-        as! [EKCalendar]
     
-    for calendar in calendars {
-        // 2
-        if calendar.title == "ioscreator" {
-            // 3
-            let startDate = NSDate()
-            // 2 hours
-            let endDate = startDate.dateByAddingTimeInterval(2 * 60 * 60)
-            
-            // 4
-            // Create Event
-            var event = EKEvent(eventStore: store)
-            event.calendar = calendar
-            
-            event.title = "New Meeting"
-            event.startDate = startDate
-            event.endDate = endDate
-            
-            // 5
-            // Save Event in Calendar
-            var error: NSError?
-            let result = store.saveEvent(event, span: EKSpanThisEvent, error: &error)
-            
-            if result == false {
-                if let theError = error {
-                    println("An error occured \(theError)")
-                }
-            }
-        }
-    }
 }
-
-
