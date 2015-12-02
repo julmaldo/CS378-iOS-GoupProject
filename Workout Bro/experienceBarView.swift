7e73cfe26b225e76256c2b59673fa920234f4d17
Julio Maldonado <Julio.A.Maldo@gmail.com>
2015-12-01 20:27:04 -0600

    working on xp bar

---
+++ Workout Bro/experienceBarView.swift
@@ -0,0 +1,77 @@
+//
+//  experienceBarView.swift
+//  Workout Bro
+//
+//  Created by Julio Maldonado on 12/1/15.
+//  Copyright © 2015 JEC INC. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable class experienceBarView: UIView {
+
+    @IBInspectable var counterTotal: Int = NoOfGlasses
+    @IBInspectable var counter: Int = 0 {
+        didSet {
+            if counter <=  NoOfGlasses {
+                setNeedsDisplay()
+            }
+        }
+    }
+    @IBInspectable var outlineColor: UIColor = UIColor.blueColor()
+    @IBInspectable var counterColor: UIColor = UIColor.orangeColor()
+    
+    override func drawRect(rect: CGRect) {
+        
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        let arcWidth: CGFloat = 76
+        
+        let startAngle: CGFloat = 3 * π / 4
+        let endAngle: CGFloat = π / 4
+        
+        let path = UIBezierPath(arcCenter: center,
+            radius: radius/2 - arcWidth/2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true)
+        
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
+        
+        let angleDifference: CGFloat = 2 * π - startAngle + endAngle
+        
+        let arcLengthPerGlass = angleDifference / CGFloat(NoOfGlasses)
+        
+        let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
+        
+        let outlinePath = UIBezierPath(arcCenter: center,
+            radius: bounds.width/2 - 2.5,
+            startAngle: startAngle,
+            endAngle: outlineEndAngle,
+            clockwise: true)
+        
+        outlinePath.addArcWithCenter(center,
+            radius: bounds.width/2 - arcWidth + 2.5,
+            startAngle: outlineEndAngle,
+            endAngle: startAngle,
+            clockwise: false)
+        
+        outlinePath.closePath()
+        
+        outlineColor.setStroke()
+        outlinePath.lineWidth = 5.0
+        outlinePath.stroke()
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
