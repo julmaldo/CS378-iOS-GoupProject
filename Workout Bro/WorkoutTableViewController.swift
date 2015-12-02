af6c648b15b977e6b26bbe6bf40efa71c81c592b
Julio Maldonado <Julio.A.Maldo@gmail.com>
2015-12-01 20:58:58 -0600

    removed top bar on main view controller

--- Workout Bro/WorkoutTableViewController.swift
+++ Workout Bro/WorkoutTableViewController.swift
@@ -14,6 +14,7 @@ class WorkoutTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = false
     }
     
     override func didReceiveMemoryWarning() {
