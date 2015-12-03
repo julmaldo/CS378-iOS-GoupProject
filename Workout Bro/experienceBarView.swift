0bd5541bfb0dfcb734258dc6eb1b2edf7c4430fb
Julio Maldonado <Julio.A.Maldo@gmail.com>
2015-12-02 22:02:58 -0600

    working on progressBar

--- Workout Bro/experienceBarView.swift
+++ Workout Bro/experienceBarView.swift
@@ -26,79 +26,19 @@ import CoreData
     @IBInspectable var outlineColor: UIColor = UIColor.blueColor()
     @IBInspectable var counterColor: UIColor = UIColor.orangeColor()
     
-    
-    func loadLevel(){
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        
-        let managedContext = appDelegate.managedObjectContext
-        
-        let fetchRequest = NSFetchRequest(entityName:"Bro")
-        
-        var fetchedResults:[NSManagedObject]? = nil
-        
-        do {
-            try fetchedResults = managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject]
-        } catch {
-            // what to do if an error occurs?
-            let nserror = error as NSError
-            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-            abort()
-        }
-        
-        if let results = fetchedResults {
-            bros = results
-        } else {
-            print("Could not fetch")
-        }
-        if bros.count > 0{
-            let bro = bros[0]
-            counterTotal = Int(bro.valueForKey("experience") as! String)!
-        }
-    }
-    
     override func drawRect(rect: CGRect) {
-        
-        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
-        let radius: CGFloat = max(bounds.width, bounds.height)
-        let arcWidth: CGFloat = 76
-        
-        let startAngle: CGFloat = 3 * π / 4
-        let endAngle: CGFloat = π / 4
-        
-        let path = UIBezierPath(arcCenter: center,
-            radius: radius/2 - arcWidth/2,
-            startAngle: startAngle,
-            endAngle: endAngle,
-            clockwise: true)
-        
-        path.lineWidth = arcWidth
-        counterColor.setStroke()
-        path.stroke()
-        
-        let angleDifference: CGFloat = 2 * π - startAngle + endAngle
-        
-        let arcLengthPerGlass = angleDifference / CGFloat(counterTotal * 3)
-        
-        let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
-        
-        let outlinePath = UIBezierPath(arcCenter: center,
-            radius: bounds.width/2 - 2.5,
-            startAngle: startAngle,
-            endAngle: outlineEndAngle,
-            clockwise: true)
-        
-        outlinePath.addArcWithCenter(center,
-            radius: bounds.width/2 - arcWidth + 2.5,
-            startAngle: outlineEndAngle,
-            endAngle: startAngle,
-            clockwise: false)
-        
-        outlinePath.closePath()
-        
-        outlineColor.setStroke()
-        outlinePath.lineWidth = 5.0
-        outlinePath.stroke()
+//        let context = UIGraphicsGetCurrentContext()
+//        CGContextSetLineWidth(context, 4.0)
+//        CGContextSetStrokeColorWithColor(context,
+//            UIColor.blueColor().CGColor)
+//        let rectangle = CGRectMake(12,170,200,80)
+//        CGContextAddRect(context, rectangle)
+//        CGContextStrokePath(context)
+//        CGContextSetFillColorWithColor(context,
+//            UIColor.redColor().CGColor)
+//        CGContextFillRect(context, rectangle)
     }
+    
 
     /*
     // Only override drawRect: if you perform custom drawing.
